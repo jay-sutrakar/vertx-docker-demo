e0f48693551327860b6cf4ec644ed068a99b1db8
jaysutrakar <jaisutrakar@gmail.com>
2023-11-15 14:58:14 +0530

    practice(docker-demo):added get endpoint in hello verticle

--- src/main/java/docker/HelloVerticle.java
+++ src/main/java/docker/HelloVerticle.java
@@ -1,13 +1,21 @@
 package docker;
 
 import io.vertx.core.AbstractVerticle;
+import io.vertx.ext.web.Router;
 
 public class HelloVerticle extends AbstractVerticle {
     @Override
     public void start() throws Exception {
-        vertx.createHttpServer().requestHandler(request -> {
-            System.out.println("server is started");
-            request.response().end("Hello java world");
-        }).listen(8080);
+        Router router = Router.router(vertx);
+        router.get("/")
+                .handler(rc -> {
+                    rc.response().setStatusCode(200)
+                            .end("Hello from vertx docker demo.");
+                });
+        vertx.createHttpServer()
+                .requestHandler(router)
+                .listen(8080)
+                .onSuccess(rs -> System.out.println("Vertx server started."))
+                .onFailure(Throwable::printStackTrace);
     }
 }
